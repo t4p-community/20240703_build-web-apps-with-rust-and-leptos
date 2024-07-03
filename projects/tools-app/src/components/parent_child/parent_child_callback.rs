950b3da34f46a31ed4cf32fb6b502440081b69a0
Training 4 Programmers <167525767+t4p-community@users.noreply.github.com>
2024-07-03 16:26:54 +0000

    add parent-child demo 2

--- projects/tools-app/src/components/parent_child/parent_child_callback.rs
+++ projects/tools-app/src/components/parent_child/parent_child_callback.rs
@@ -1,4 +1,27 @@
+use ev::MouseEvent;
 use leptos::*;
+
+#[component]
+pub fn Child(
+    counter: ReadSignal<i16>,
+    #[prop(into)] on_increment: Callback<MouseEvent>,
+    #[prop(into)] on_decrement: Callback<MouseEvent>,
+) -> impl IntoView {
+    view! {
+        <div style="border: 1px solid black;margin: 4px">
+            <h3>"Child Callback"</h3>
+            <p>"Counter: " {counter}</p>
+            <div>
+                <button type="button" on:click=on_increment>
+                    "Child +1"
+                </button>
+                <button type="button" on:click=on_decrement>
+                    "Child -1"
+                </button>
+            </div>
+        </div>
+    }
+}
 
 #[component]
 pub fn Parent() -> impl IntoView {
@@ -22,6 +45,7 @@ pub fn Parent() -> impl IntoView {
                     </button>
                 </div>
             </div>
+            <Child counter=counter on_increment=increment_counter on_decrement=decrement_counter/>
         </div>
     }
 }
